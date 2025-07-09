7051a42371ce47a12f72cf00baa021055dbef1d7
TechopsTTU <techops@ttu.com>
2025-07-08 18:08:26 -0700

    Add tools app URLs and tool_list.html template

---
+++ tools/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'tools'
+
+urlpatterns = [
+    path('', views.ToolListView.as_view(), name='tool_list'),
+    path('tool/<int:pk>/', views.ToolDetailView.as_view(), name='tool_detail'),
+    path('tool/add/', views.ToolCreateView.as_view(), name='tool_add'),
+    path('tool/<int:pk>/edit/', views.ToolUpdateView.as_view(), name='tool_edit'),
+    path('tool/<int:pk>/delete/', views.ToolDeleteView.as_view(), name='tool_delete'),
+]
--- tools/views.py
+++ tools/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Tool
 
-# Create your views here.
+class ToolListView(ListView):
+    model = Tool
+    template_name = 'tools/tool_list.html'
+
+class ToolDetailView(DetailView):
+    model = Tool
+    template_name = 'tools/tool_detail.html'
+
+class ToolCreateView(CreateView):
+    model = Tool
+    fields = ['name', 'serial_number', 'calibrated', 'last_checked_in']
+    template_name = 'tools/tool_form.html'
+    success_url = reverse_lazy('tools:tool_list')
+
+class ToolUpdateView(UpdateView):
+    model = Tool
+    fields = ['name', 'serial_number', 'calibrated', 'last_checked_in']
+    template_name = 'tools/tool_form.html'
+    success_url = reverse_lazy('tools:tool_list')
+
+class ToolDeleteView(DeleteView):
+    model = Tool
+    template_name = 'tools/tool_confirm_delete.html'
+    success_url = reverse_lazy('tools:tool_list')
